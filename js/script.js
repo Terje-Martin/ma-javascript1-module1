7cebb002a805d2b0c273fb05b9c752fefceed639
xAureum <91454735+TerjeMartin@users.noreply.github.com>
2022-01-26 14:21:28 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+// Question 1
+// let cat = {
+//     complain: "Meow!"
+// };
+// console.log(cat.complain);
+
+// Question 2
+// const heading = document.querySelector("h3");
+// heading.innerHTML = "Updated heading";
+
+// Question 3
+// const heading = document.querySelector("h3");
+// heading.innerHTML = "Updated heading";
+// heading.style.fontSize = "2em";
+
+// Question 4
+// const heading = document.querySelector("h3");
+// heading.innerHTML = "Updated heading";
+// heading.style.fontSize = "2em";
+// heading.classList.add("subheading");
+
+// Question 5
+// var paragrahps = document.getElementsByTagName("p");
+// var colors = ["red"];
+
+// for(var i=0 ; i < paragrahps.length; i++) {
+//     paragrahps[i].style.background = colors;
+
+// }
+
+// Question 6
+// const resultsContainer = document.getElementsByClassName("results");
+// resultsContainer.innerHTML = "<p>New paragraph</p>";
+// resultsContainer.backgroundColor = "yellow";
+
+// Question 7
 const cats = [
     {
         name: "Blob",
@@ -12,3 +48,35 @@ const cats = [
     }
 ];
 
+
+// function list(catlist) {
+//     for(let i = 0; i < catlist.length; i++) {
+//         console.log(catlist[i]);
+//     }
+// }
+// list(cats);   
+
+
+// Question 8
+
+// function createCats(cats) {
+
+//     let html = "";
+
+//     for(let i = 0; i < createCats.length; i++) {
+
+//         let age = "Age unknown";
+
+//         html += `<div class="cat-container">
+//                     <h5>${cats[i].name}</h5>
+//                     <p>Age: ${age}</p>
+//                 </div>`;
+//     }
+
+//     return html;
+// }
+
+// const newHTML = createCats(cats);
+
+// const createCats = document.querySelector(".cat-container");
+// cats.innerHTML = newHTML;
